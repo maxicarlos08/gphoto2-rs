05b427e975e9a7a104f61af461a3bca41cd19e70
Maxicarlos08 <maxicarlos08@gmail.com>
2022-10-01 22:02:30 +0200

    added locale testing example

---
+++ examples/locales.rs
@@ -0,0 +1,20 @@
+//! Run this example with your system locale set to something non UTF-8.
+
+use gphoto2::{Context, Result, widget::TextWidget};
+
+const CONFIG_KEY: &str = "d406";
+const NON_UNICODE_VALUE: &str = "貓"; // "cat" in chinese (because they are awesome)
+
+fn main() -> Result<()> {
+  let camera = Context::new()?.autodetect_camera()?;
+
+  let text_widget: TextWidget = camera.config_key(CONFIG_KEY)?;
+  text_widget.set_value(NON_UNICODE_VALUE)?;
+
+  camera.set_config(&text_widget)?;
+
+  let text_widget: TextWidget = camera.config_key(CONFIG_KEY)?;
+
+  assert_eq!(text_widget.name(), NON_UNICODE_VALUE);
+  Ok(())
+}
